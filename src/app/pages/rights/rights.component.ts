4fbebde3841b9a217bcd8664f085da1482862d3e
shubhojitroy <royshubhojit@gmail.com>
2025-04-25 12:07:11 +1000

    updating the main of brd0lp-app

---
+++ src/app/pages/rights/rights.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rights',
+  imports: [],
+  templateUrl: './rights.component.html',
+  styleUrl: './rights.component.scss'
+})
+export class RightsComponent {
+
+}
