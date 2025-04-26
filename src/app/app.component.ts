a65910cb107b520f7699502e2d4363d55d950663
shubhojitroy <royshubhojit@gmail.com>
2025-04-26 10:30:08 +1000

    update 4th wrk after initial update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactComponent } from './aside/contact.component';
+import { CardComponent } from './shared/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, ContactComponent, RouterOutlet],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    CardComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
